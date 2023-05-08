d977607cf3bdbdff90b1cc7121db4003d3913b1f
Fercho2309 <efibarra67@misena.edu.co>
2023-05-08 15:42:53 -0500

    rutas salas

--- src/api/Salas.py
+++ src/api/Salas.py
@@ -26,3 +26,43 @@ def Sala():
         return jsonify(result_sala)
     else:
         return vf
+    
+#---------SAVE/CREAR------------
+@routes_salas.route('/save_sala', methods=['POST'])
+def save_sala():
+    nombre_sala = request.json['nombre_sala']
+    capacidad = request.json['capacidad']
+    new_room = salas(nombre_sala,capacidad)
+    db.session.add(new_room)
+    db.session.commit()
+    return redirect ("/Tsala")
+
+#------------DELETE/ELIMINAR------------
+@routes_salas.route('/delete_salas/<id>', methods=['GET'])
+def delete_salas(id):
+    print(id)
+    room = salas.query.get(id)
+    mensaje = {}
+    if(room):    
+        db.session.delete(room)
+        db.session.commit()
+        mensaje = "Dato eliminado"
+    else:
+        mensaje = "dato no encontrado"
+    response = {
+        'status': 200,
+        'body': mensaje
+    }
+    return jsonify(response)
+
+#------------UPDATE/ACTUALIZAR-----------
+@routes_salas.route('/update_sala', methods=['POST'])
+def update_sala():
+    id = request.json['id']
+    nombre_sala = request.json['nombre_sala']
+    capacidad = request.json['capacidad']
+    room = salas.query.get(id)
+    room.nombre_sala = nombre_sala
+    room.capacidad = capacidad
+    db.session.commit()
+    return redirect ('/Tsala') 
