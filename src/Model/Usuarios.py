e3298c8b4bc5166adb7d7c6f82cac83692c22427
luxITD <luismardur@gmail.com>
2023-05-18 14:39:23 -0500

    usuario

--- src/Model/Usuarios.py
+++ src/Model/Usuarios.py
@@ -9,13 +9,8 @@ class usuarios(db.Model):
     correo_electronico = db.Column(db.String(50))
     contrasena = db.Column(db.String(50))
 
-<<<<<<< HEAD
     def __init__(self, Rol, nombre, correo_electronico, contraseña):
         self.Rol = Rol
-=======
-    def __init__(self, id_roles_usuarios, nombre, correo_electronico, contrasena):
-        self.id_roles_usuarios = id_roles_usuarios
->>>>>>> 6cc481dac2b57a41ba0fc38cab2e5d0189e8896d
         self.nombre = nombre
         self.correo_electronico = correo_electronico
         self.contrasena = contrasena
@@ -25,8 +20,4 @@ with app.app_context():
 
 class usuariosSchema(ma.Schema):
     class Meta:
-<<<<<<< HEAD
         fields = ('id','Rol','nombre','correo_electronico','contraseña')
-=======
-        fields = ('id','id_roles_usuarios','nombre','correo_electronico','contrasena')
->>>>>>> 6cc481dac2b57a41ba0fc38cab2e5d0189e8896d
