58190a1669a68a1a0bec19d4c7bd987076a4ea35
luxITD <luismardur@gmail.com>
2023-05-27 12:50:48 -0500

    js ticket

--- src/config/static/js/BIJS/asientos.js
+++ src/config/static/js/BIJS/asientos.js
@@ -4,6 +4,8 @@ function mostrarTicket() {
     const sala = document.getElementById('sala');
     const funcion = document.getElementById('funcion');
 
+    
+
     axios.get('/fronted/mostrarticket', {
         responseType: 'json',
 
@@ -15,26 +17,26 @@ function mostrarTicket() {
             let opciones = '';
             for (let index = 1; index < lenght; index++) {
                 opciones += `
-            <div class="card" id="contenedor_ticket">
-      <title>Generador de Ticket de Cine</title>
-      </style>
-      <div class="container" id="formulario">
-        <div class="card" id="formulario">
-          <h3 class="card-title">Generador de Ticket de Cine</h3>
-          <form id="ticketForm">
-            <div class="form-group">
-              <label for="pelicula">Película:</label>
-              <input type="" class="form-control" id="pelicula" required>
-            </div>
-            <div class="form-group">
-              <label for="sala">Sala:</label>
-              <input type="text" class="form-control" id="sala" required>
-            </div>
-            <div class="form-group">
-              <label for="funcion">Función:</label>
-              <input type="text" class="form-control" id="funcion" required>
-            </div>
-            `
+                <div class="card" id="contenedor_ticket">
+                <title>Generador de Ticket de Cine</title>
+                </style>
+                <div class="container" id="formulario">
+                  <div class="card" id="formulario">
+                    <h3 class="card-title">Generador de Ticket de Cine</h3>
+                    <form id="ticketForm">
+                      <div class="form-group">
+                        <label for="pelicula">Película:</label>
+                        <input type="text" class="form-control" id="pelicula" required readonly>
+                      </div>
+                      <div class="form-group">
+                        <label for="sala">Sala:</label>
+                        <input type="text" class="form-control" id="sala" required readonly>
+                      </div>
+                      <div class="form-group">
+                        <label for="funcion">Función:</label>
+                        <input type="text" class="form-control" id="funcion" required readonly>
+                      </div>
+                      `
             }
         })
 }
