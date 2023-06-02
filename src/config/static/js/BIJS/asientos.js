9a01c567d87bfae315d127471a7e39440a54f5ec
luxITD <luismardur@gmail.com>
2023-06-02 16:46:14 -0500

    updat

--- src/config/static/js/BIJS/asientos.js
+++ src/config/static/js/BIJS/asientos.js
@@ -5,8 +5,20 @@ const funcion = document.getElementById('funcion');
 function mostrarTicket() {
 axios.get('/mostrarticket',{
     responseType: 'json'
+    
 })  
 
 .then (funcion(response)
+
 )
 }
+
+function genPDF() {
+    var doc = new jsPDF();
+    var container = document.getElementById('contenedor_ticket');
+    doc.fromHTML(container.innerHTML, 15, 15, {
+        'width': 170,
+      });
+    doc.save('saveus.pdf')
+  
+  }
--- src/config/static/js/scriptasientos.js
+++ src/config/static/js/scriptasientos.js
@@ -35,3 +35,59 @@ container.addEventListener('click', (e) => {
 movieSelect.addEventListener('change', (e) => {
   // Aquí puedes realizar cualquier acción cuando cambie la selección de película
 });
+
+// Variables globales
+var botonesAsientos = document.querySelectorAll('.asiento');
+var botonComprar = document.querySelector('.comprar');
+
+// Agregar un evento de clic al botón de "Comprar"
+botonComprar.addEventListener('click', function() {
+  var botonesSeleccionados = document.querySelectorAll('.asiento-seleccionado');
+
+  // Verificar si se han seleccionado asientos
+  if (botonesSeleccionados.length > 0) {
+    // Obtener información de los asientos seleccionados
+    var asientosSeleccionados = [];
+    botonesSeleccionados.forEach(function(botonSeleccionado) {
+      var fila = botonSeleccionado.dataset.fila;
+      var asiento = botonSeleccionado.dataset.asiento;
+      asientosSeleccionados.push({fila: fila, asiento: asiento});
+    });
+
+    // Enviar información a un servidor utilizando Axios
+    axios.post('/guardar-asientos', {
+      asientos: asientosSeleccionados
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  }
+});
+
+// Agregar un evento de clic a cada botón de asiento
+botonesAsientos.forEach(function(botonAsiento) {
+  botonAsiento.addEventListener('click', function() {
+    // Cambiar la clase del botón para reflejar el estado de selección
+    if (botonAsiento.classList.contains('asiento-seleccionado')) {
+      botonAsiento.classList.remove('asiento-seleccionado');
+      botonAsiento.textContent = botonAsiento.dataset.asiento;
+    } else if (botonAsiento.classList.contains('asiento-ocupado')) {
+      // El asiento ya está ocupado, no se puede seleccionar
+      return;
+    } else {
+      botonAsiento.classList.add('asiento-seleccionado');
+      botonAsiento.textContent = 'X';
+    }
+    
+    // Verificar si se han seleccionado asientos para habilitar/deshabilitar el botón de "Comprar"
+    var botonesSeleccionados = document.querySelectorAll('.asiento-seleccionado');
+    if (botonesSeleccionados.length > 0) {
+      botonComprar.disabled = false;
+    } else {
+      botonComprar.disabled = true;
+    }
+  });
+});
