ff5c919e311ffc564b6adc5a766389c7f0777812
DainerB <dainerbolano16@gmail.com>
2023-06-08 14:05:36 -0500

    New Block body, Head and Footer in LayoutMainLogin. Update name user in class='user-menu'

--- src/config/static/js/BIJS/userDetail.js
+++ src/config/static/js/BIJS/userDetail.js
@@ -12,6 +12,10 @@ function getUserLogged() {
 
 .then(function(res){ 
     const user = res.data.user
+    const nameUser = document.getElementById("Update-nameUser")
+    nameUser.innerHTML = `
+    <span id="Update-nameUser" class="user-name" onclick="toggleDropdown()">${user}</span>
+    `
 })
 .catch((err) => {
     console.log(err);
