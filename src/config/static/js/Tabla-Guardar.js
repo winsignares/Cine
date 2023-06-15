96ba71994c5350da1f9d0333b9d3128f64b63b2c
darell12 <darellorlandoe@gmail.com>
2023-06-15 15:50:03 -0500

    Update table-admin | No Funcional.Aun

--- src/config/static/js/Tabla-Guardar.js
+++ src/config/static/js/Tabla-Guardar.js
@@ -1,4 +1,4 @@
-// Capturar el formulario por su ID
+/*// Capturar el formulario por su ID
 var form = document.getElementById('formulario');
 // 
 
@@ -30,4 +30,89 @@ form.addEventListener('submit', function(event) {
 
   // Limpiar los campos del formulario
   form.reset();
-});
+});*/
+function showAllPeliculas() {
+  axios.get("/showAdmin", {
+      id: id,
+      titulo: titulo,
+      genero: genero,
+      duracion: duracion,
+      sinopsis: sinopsis,
+      director: director,
+      imagen: imagen,
+    })
+    .then(function (res) {
+      datos = res.data;
+      console.log(res.datos);
+      // Crear una nueva fila en la tabla
+      var tableBody = document.getElementById("peliculas-lista");
+      var newRow = tableBody.insertRow();
+      newRow.innerHTML = `
+        <td>${datos.id}</td>
+        <td>${datos.titulo}</td>
+        <td>${datos.genero}</td>
+        <td>${datos.duracion}</td>
+        <td>${datos.sinopsis}</td>
+        <td>${datos.director}</td>
+        <td>${datos.imagen}</td>
+      `;
+
+      // Limpiar los campos del formulario
+      form.reset();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+function sendNewPeliculas() {
+  // Obtener los valores de los campos del formulario
+  var id = document.getElementById("id").value;
+  var titulo = document.getElementById("titulo").value;
+  var genero = document.getElementById("genero").value;
+  var duracion = document.getElementById("duracion").value;
+  var sinopsis = document.getElementById("sinopsis").value;
+  var director = document.getElementById("director").value;
+  var imagen = document.getElementById("imagen").value;
+
+  axios.post("/saveAdmin", {
+      id: id,
+      titulo: titulo,
+      genero: genero,
+      duracion: duracion,
+      sinopsis: sinopsis,
+      director: director,
+      imagen: imagen,
+    })
+    .then(function (res) {
+      datos = res.data;
+      console.log(res.datos);
+      //showAllPeliculas(datos)
+      // Crear una nueva fila en la tabla
+      var tableBody = document.getElementById("peliculas-lista");
+      var newRow = tableBody.insertRow();
+      newRow.innerHTML = `
+        <td>${datos.id}</td>
+        <td>${datos.titulo}</td>
+        <td>${datos.genero}</td>
+        <td>${datos.duracion}</td>
+        <td>${datos.sinopsis}</td>
+        <td>${datos.director}</td>
+        <td>${datos.imagen}</td>
+      `;
+
+      // Limpiar los campos del formulario
+      //form.reset();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+/*/ Capturar el formulario por su ID
+var form = document.getElementById("formulario");
+//
+
+// Agregar un evento para escuchar el envío del formulario
+form.addEventListener("click", function (event) {
+  event.preventDefault(); // Evitar el envío por defecto
+  sendNewPeliculas();
+});/*/
--- src/rutas/Admin.py
+++ src/rutas/Admin.py
@@ -1,5 +1,6 @@
 from config.db import db, app, ma
 from flask import Blueprint, Flask,  redirect, request, jsonify, json, session, render_template
+from Model.peliculas import peliculas, peliculasSchema
 routes_Admin = Blueprint("routes_Admin", __name__)
 
 
@@ -7,3 +8,41 @@ routes_Admin = Blueprint("routes_Admin", __name__)
 def indexAdmin():
     
     return render_template('/PeliculasWeb-main/Admin.html')
+@routes_Admin.route('/saveAdmin', methods=['GET'] )
+def saveAdmin():
+    
+    id = request.json['id']
+    titulo = request.json['titulo']
+    genero = request.json['genero']
+    duracion = request.json['duracion']
+    sinopsis = request.json['sinopsis']
+    director = request.json['director']
+    imagen = request.json['imagen']
+    
+    # Crear una nueva instancia de usuarios con los datos proporcionados y el token generado
+    new_pelis = peliculas(id,titulo, genero, duracion, sinopsis, director, imagen)
+    
+    # Agregar el nuevo usuario a la sesión y guardar los cambios en la base de datos
+    db.session.add(new_pelis)
+    db.session.commit()
+    # return '/showAdmin'
+
+# @routes_Admin.route('/showAdmin', methods=['GET'] )
+# def showAdmin():
+    # Retornar los datos ingresados a la BD
+    #descpeli = db.Model.metadata.tables['tblpeliculas'] 
+    peliculasDB = db.session.query(peliculas).all()
+    i = 0
+    for peli in peliculasDB:
+        i += 1
+        datos = {
+            'id': peli.id,
+            'titulo': peli.titulo,
+            'genero': peli.genero,
+            'duracion': peli.duracion,
+            'sinopsis': peli.sinopsis,
+            'director': peli.director,
+            'imagen': peli.imagen,
+            'video': peli.video
+        }
+    return jsonify(datos)
